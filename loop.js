68d0f28b08043ed2a218ff94eca6859c922835ed
Matheus094-tech <matheusbarbosa094@gmail.com>
2023-07-15 18:24:11 -0300

    Ajustando padrão

---
+++ loop.js
@@ -0,0 +1,15 @@
+let button = document.getElementById('no');
+let height = window.innerHeight - 50;
+let width = window.innerWidth - 50;
+
+button.addEventListener('mouseover', function () {
+button.style.position = "absolute";
+button.style.top = Math.random() * height + "px";
+button.style.left = Math.random() * width + "px";
+})
+
+let buttonYes = document.getElementById('si');
+
+buttonYes.addEventListener('click', function () {
+window.alert('Obrigado por assumir, concordamos 👍')
+})
